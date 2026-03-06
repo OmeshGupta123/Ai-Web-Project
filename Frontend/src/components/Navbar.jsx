05eceb34f38181a542f9c504c3219815e2f7a557
Omesh Gupta <omeshkoshal@gmail.com>
2026-03-06 16:49:30 +0530

    add guest mode feature, Remove premium and free tier, fix api issues, fix watch demo section, change logo and other changes.

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -9,14 +9,14 @@ const Navbar = () => {
     const {user} = useUser();
     const {openSignIn} = useClerk();
   return (
-    <div className='fixed z-5 w-full background-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32 cursor-pointer'>
-        <img src={assets.logo} alt="logo" className='w-32 sm:w-44' onClick={() => {
+    <div className='fixed z-5 w-full background-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32 cursor-pointer drop-shadow-2xl'>
+        <img src={assets.logo} alt="logo" className='w-46 sm:w-66'  onClick={() => {
             navigate('/')
         }} />
 
         {
           user ? <UserButton /> : (
-        <button onClick={openSignIn} className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5'>Get Started <ArrowRight className='w-4 h-4' /></button>
+        <button onClick={openSignIn} className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5'>Get Started <ArrowRight className='w-8 h-8' /></button>
           )
         }
 
